569a53def004be86e64757979c1324a6d1805ecd
reetika97 <raoreetika16@gmail.com>
2024-07-19 15:56:23 +0200

    test multi atom verlet types.h

--- src/verlet.cpp
+++ src/verlet.cpp
@@ -4,6 +4,7 @@
 
 #include "header_files/verlet.h"
 
+//Single Atom Velocity Verlet
 void verlet_step1(double &x, double &y, double &z, double &vx, double &vy, double &vz,
                   double fx, double fy, double fz, double timestep, double mass) {
 
@@ -37,5 +38,57 @@ void single_atom_verlet(double &x, double &y, double &z, double &vx, double &vy,
         verlet_step1(x, y, z, vx, vy, vz, fx, fy, fz, timestep, mass);
 
         verlet_step2(vx, vy, vz, fx, fy, fz, timestep, mass);
+    }
+}
+
+//Multi Atom Velocity Verlet
+void verlet_step1(Eigen::Array3Xd &positions, Eigen::Array3Xd &velocities,
+                  const Eigen::Array3Xd &forces, double timestep, double mass){
+
+    // Velocity update
+    auto vx{velocities.row(0)};
+    auto vy{velocities.row(1)};
+    auto vz{velocities.row(2)};
+    auto fx{forces.row(0)};
+    auto fy{forces.row(1)};
+    auto fz{forces.row(2)};
+    auto x{positions.row(0)};
+    auto y{positions.row(1)};
+    auto z{positions.row(2)};
+
+    vx += 0.5 * fx * timestep / mass;
+    vy += 0.5 * fy * timestep / mass;
+    vz += 0.5 * fz * timestep / mass;
+
+    // Position update
+    x += vx * timestep;
+    y += vy * timestep;
+    z += vz * timestep;
+
+}
+
+void verlet_step2(Eigen::Array3Xd &velocities, const Eigen::Array3Xd &forces,
+                  double timestep, double mass) {
+
+    auto vx{velocities.row(0)};
+    auto vy{velocities.row(1)};
+    auto vz{velocities.row(2)};
+    auto fx{forces.row(0)};
+    auto fy{forces.row(1)};
+    auto fz{forces.row(2)};
+
+    vx += 0.5 * fx * timestep / mass;
+    vy += 0.5 * fy * timestep / mass;
+    vz += 0.5 * fz * timestep / mass;
+}
+
+void multi_atom_verlet(Eigen::Array3Xd &positions, Eigen::Array3Xd &velocities,
+                       const Eigen::Array3Xd &forces, double timestep, double mass, int nb_steps){
+    //Integrator for nb_steps
+    for (int i = 0; i < nb_steps; ++i) {
+
+        verlet_step1(positions, velocities, forces, timestep, mass);
+
+        verlet_step2(velocities, forces, timestep, mass);
     }
 }
--- tests/test_verlet_integrator.cpp
+++ tests/test_verlet_integrator.cpp
@@ -3,10 +3,10 @@
 //
 
 #include "header_files/verlet.h"
+#include "header_files/types.h"
 #include <gtest/gtest.h>
 #include <iostream>
 #include <cstdlib>  // For rand() and srand()
-#include <ctime>    // For time()
 #include <cmath>
 
 // Function to generate a random float
@@ -56,5 +56,57 @@ TEST(VerletTest, RandomTest) {
 
     EXPECT_NEAR(r_verlet, r_analytical, 1e-6);
     EXPECT_NEAR(v_verlet, v_analytical, 1e-6);
+
+}
+
+
+// Multi atom VerletTest
+TEST(VerletTest, MultiAtomTest) {
+
+    Eigen::ArrayXd vel(3);
+    Eigen::ArrayXd pos(3);
+    Eigen::ArrayXd f(3);
+
+    pos(0) = generateRandomFloat(); pos(1)= generateRandomFloat(); pos(2)= generateRandomFloat();
+    vel(0) = generateRandomFloat(); vel(1)= generateRandomFloat(); vel(2)= generateRandomFloat();
+    f(0) = generateRandomFloat(); f(1)= generateRandomFloat(); f(2)= generateRandomFloat();
+
+    int nb_steps = 1000;
+    double timestep, mass;
+    timestep = 1e-15; mass = 3.27;
+
+    int nb_atoms = 4;
+    Positions_t positions(3, nb_atoms);
+    Velocities_t velocities(3, nb_atoms);
+    Forces_t forces(3, nb_atoms);
+
+
+    auto vx{velocities.row(0)};
+    auto vy{velocities.row(1)};
+    auto vz{velocities.row(2)};
+    auto fx{forces.row(0)};
+    auto fy{forces.row(1)};
+    auto fz{forces.row(2)};
+    auto x{positions.row(0)};
+    auto y{positions.row(1)};
+    auto z{positions.row(2)};
+
+    vx = vel(0); vy = vel(1); vz = vel(2);
+    x = pos(0); y = pos(1); z = pos(2);
+    fx = f(0); fy = f(1); fz = f(2);
+
+
+    single_atom_verlet(pos(0), pos(1), pos(2), vel(0),
+                       vel(1), vel(2), f(0), f(1), f(2),
+                       timestep, mass, nb_steps);
+
+    multi_atom_verlet(positions, velocities, forces, timestep, mass, nb_steps);
+
+    EXPECT_NEAR(velocities(0), vel(0), 1e-6);
+    EXPECT_NEAR(velocities(1), vel(1), 1e-6);
+    EXPECT_NEAR(velocities(2), vel(2), 1e-6);
+    EXPECT_NEAR(positions(0), pos(0), 1e-6);
+    EXPECT_NEAR(positions(1), pos(1), 1e-6);
+    EXPECT_NEAR(positions(2), pos(2), 1e-6);
 
 }
