c3c55b742a5a2e1d20d3fb5308a1fb4245440679
reetika97 <raoreetika16@gmail.com>
2024-07-19 15:56:12 +0200

    test multi atom verlet types.h

---
+++ src/header_files/types.h
@@ -0,0 +1,17 @@
+//
+// Created by raor on 19.07.24.
+//
+
+#ifndef TYPES_H
+#define TYPES_H
+
+#include <Eigen/Dense>
+using Positions_t = Eigen::Array3Xd;
+using Velocities_t = Eigen::Array3Xd;
+using Forces_t = Eigen::Array3Xd;
+using Names_t = std::vector<std::string>;
+using Masses_t = Eigen::ArrayXd;
+using Energies_t = Eigen::ArrayXd;
+
+
+#endif // TYPES_H
